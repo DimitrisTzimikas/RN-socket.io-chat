d87a034a4e810c03a55c6ba9b68f48c8faa49cfb
TZIMIKAS Dimitris <tzimikasd@gmail.com>
2020-12-17 17:10:14 +0200

    change names

--- mobile-app/src/App.js
+++ mobile-app/src/App.js
@@ -1,8 +1,15 @@
-import React, { Component }                                                         from 'react';
-import { YellowBox, Text, View, ScrollView, TouchableOpacity, TextInput, Keyboard } from 'react-native';
-import io                                                                           from 'socket.io-client';
-import { container, button, scrollView, text, input }                               from './styles';
-
+import React, { Component } from 'react';
+import {
+  YellowBox,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  TextInput,
+  Keyboard,
+} from 'react-native';
+import io from 'socket.io-client';
+import { container, button, scrollView, text, input } from './styles';
 
 YellowBox.ignoreWarnings([
   'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?',
@@ -20,28 +27,24 @@ const url = 'https://3eed2986.ngrok.io';
  ================================ */
 class App extends Component {
   state = {
-    textList: [
-      { text: '' },
-    ],
+    textList: [{ text: '' }],
     input: '',
   };
-  
+
   constructor() {
     super();
-    
+
     this.socket = io(url, { json: false });
     this.socket.on('chat message', this.onReceiveMessage);
   }
-  
-  onReceiveMessage = data => this.setState({
-    textList: [
-      ...this.state.textList,
-      { text: data },
-    ],
-  });
-  
+
+  onReceiveMessage = data =>
+    this.setState({
+      textList: [...this.state.textList, { text: data }],
+    });
+
   onTextInput = input => this.setState({ input });
-  
+
   onButtonPress = () => {
     if (this.state.input !== '') {
       this.socket.emit('chat message', this.state.input);
@@ -49,24 +52,28 @@ class App extends Component {
       Keyboard.dismiss();
     }
   };
-  
+
   showMessage = message => <Text style={text.style}>{message.text}</Text>;
-  
+
   render() {
     return (
       <View style={container.style}>
         <ScrollView style={scrollView.style}>
           {this.state.textList.map(this.showMessage)}
         </ScrollView>
-        
+
         <TextInput
           style={input.style}
           onChangeText={this.onTextInput}
           value={this.state.input}
           onSubmitEditing={this.onButtonPress}
         />
-        
-        <TouchableOpacity style={button.container} onPress={this.onButtonPress} disabled={this.state.input === ''}>
+
+        <TouchableOpacity
+          style={button.container}
+          onPress={this.onButtonPress}
+          disabled={this.state.input === ''}
+        >
           <Text style={button.text}>Send</Text>
         </TouchableOpacity>
       </View>
